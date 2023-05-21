5b369c52336c4b1a23d75041c860e098fab3b4c1
codeylee05 <mlefa5002@gmail.com>
2023-05-21 13:52:59 +0200

    Delete codetesting.py

--- codetesting.py
+++
@@ -1,6 +0,0 @@
-#Write a program that outputs a sentence-of-the-day in Spanish
-#The program should allow the user to try translating the sentence into english should they want before outputting if they are correct or not
-#The user has up to a n number of times to try before the program outputs the translation
-#If a user got the translation correct, the first time, the sentence is removed
-#If the user got the translation correct after n number of times, the sentence is slotted for reviewing after n number of days
-#If the user never got it correct, the sentence is slotted for reviewing the day after
