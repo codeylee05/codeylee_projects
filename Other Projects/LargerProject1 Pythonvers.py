8da13c14af626da6538b9b6b80967aa088343fc3
codeylee05 <mlefa5002@gmail.com>
2023-08-21 15:16:26 +0200

    Update LargerProject1 Pythonvers.py

--- Other Projects/LargerProject1 Pythonvers.py
+++ Other Projects/LargerProject1 Pythonvers.py
@@ -4,50 +4,49 @@ sentences = {
     "Yo vivo en una casa muy grande con mi mama, mi tia, y su perro":"I live in a very big house with my mom, my aunt, and her dog",
     "Hoy es una dia muy interesante porque mi companero de intercambio que viene de Francia nos visita":"Today will be a very interesting day because my exchange partner who comes from France is visiting us"
 }
-
 correct_transl = 0
-incorrec_transl = 0
+incorrect_transl = 0
 
 def userTranslation():
-        
-    user_translate = input("Translation: ")
-        
-    return user_translate
+    
+    user_transl = input("Translation: ")
+
+    return user_transl
 
 
-def checkTranslation(user_translation, actual_translation):
+def checkTranslation(user_value, actual_value):
 
-    if user_translation == actual_translation:
+    if user_value == actual_value:
         print("Correct!")
         global correct_transl
         correct_transl += 1
     else:
-        print("Incorrect!")
-        global incorrec_transl
-        incorrec_transl += 1
+        print("Incorrect")
+        global incorrect_transl
+        incorrect_transl += 1
 
-#main function#
+
 def outputSentence():
 
     i = 0
     for key, value in sentences.items():
         i += 1
-        print(f"The Spanish sentence {i} is:")
+        print("")
+        print(f"The Spanish sentence {i} is: ")
         print(f"{key}")
 
-        user_translated = userTranslation()
-        checkTranslation(user_translated, value)
+        user_transl_return = userTranslation()
+        checkTranslation(user_transl_return, value)
 
 outputSentence()
 
-
-print(f"Well done! You have gotten {correct_transl}/{len(sentences)} sentences correct")
+print("")
+print(f"Completed! You have gotten {correct_transl}/{incorrect_transl}questions correct")
+print("")
 
 def outputAll():
-    
+
     for key, value in sentences.items():
-        print(key, "|", value)
+        print(f"{key} | {value}")
 
-print(f"The sentences are: ")
 outputAll()
-
